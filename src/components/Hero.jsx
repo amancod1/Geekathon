a057b9cb0c8391d8dfd37cb0fce66af57000a70a
Aman Gupta <90703407+AmanGuptaCoder@users.noreply.github.com>
2023-11-19 09:39:44 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col '>
       <nav className='flex justify-between items-center w-full mb-10 pt-4'>
-        <img src={logo} alt='Tex-Sum' className='w-28 object-contain' />
+        <img  alt='Tex-Sum' className='w-28 object-contain' />
       
       </nav>
 
